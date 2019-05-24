5fb5540af2f1a49617c85471218cc8c15ba3cc44
WinRARRR <anthonyjj.454@gmail.com>
2019-05-23 19:38:02 -0600

    Update config.js

--- js/config.js
+++ js/config.js
@@ -44,7 +44,7 @@ Avaiable options:
 
 You can add custom backgrounds
 Just make sure it is at webm VP8 format */
-var l_background = "backgrounds/videos/inthedeep.webm";
+var l_background = "backgrounds/videos/dropsflares.webm";
 
 /*(Image-background only)
 Place one or more images
