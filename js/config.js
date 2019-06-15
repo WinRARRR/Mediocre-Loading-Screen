b41ce1aac98150b59411ba8cf2f292022f2747aa
WinRARRR <50968710+WinRARRR@users.noreply.github.com>
2019-06-15 17:32:57 -0600

    REAL CONFIG CAHNGES
    IGNORWE DA OTHER 1

--- js/config.js
+++ js/config.js
@@ -13,7 +13,7 @@ var l_serverImage = "";
 
 /*Custom server name configuration
 Leave it blank for automatic server name*/
-var l_serverName = "Welcome to Mediocre DakRP";
+var l_serverName = "Welcome to Mediocre DarkRP";
 
 /*Use a video for the background?
 Otherwise it will just use images*/
