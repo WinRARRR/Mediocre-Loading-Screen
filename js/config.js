68a054bc845f321c2781d71a5f235ff3c0c3b0b3
WinRARRR <50968710+WinRARRR@users.noreply.github.com>
2019-06-15 17:32:37 -0600

    updated congifdf
    updated congifdfdasdda

--- js/config.js
+++ js/config.js
@@ -13,7 +13,7 @@ var l_serverImage = "";
 
 /*Custom server name configuration
 Leave it blank for automatic server name*/
-var l_serverName = "Welcome to Mediocre DarkRP";
+var l_serverName = "Welcome to Mediocre DakRP";
 
 /*Use a video for the background?
 Otherwise it will just use images*/
